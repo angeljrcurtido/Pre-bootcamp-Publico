87c49ae9c2a3fb903a9b702b7225b5daf2a44827
Angel Junior <angeljrcurtido@gmail.com>
2022-12-22 15:14:44 -0300

    UPDATE

---
+++ PRACTICA_OPERADOR_TERNARIO/script.js
@@ -0,0 +1,10 @@
+let patas = 4;
+let accion = "ladra";
+
+// if (patas == 4 && accion == "ladra") {
+//     console.log ("Es un perro");
+// } else (
+//     console.log ("No es un perro")
+// )
+
+(patas == 4) && (accion == "ladra") ? console.log ("Es un perro") : console.log ("no es un perro");
